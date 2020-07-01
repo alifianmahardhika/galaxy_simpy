4a5ca24a7ce2155b8f8c5130ecdc5e22ae3845ea
hudanesia <hudanasrulloh1453@gmail.com>
2020-07-02 05:23:48 +0700

    edit file

--- vtr-plot.py
+++ vtr-plot.py
@@ -9,20 +9,21 @@ def phi_inv(q):
 def phi_inverse(q):
     return sqrt(q)*sqrt((1+sqrt(1+(4/q**2)))/2.0)
 #Global parameter
-a_0 = 10e-2
+a_0 = 10e-10
 m = 10e11 #[MO]
-R = np.arange(0.3,100,0.001) #[kpc]
+R = np.arange(0.3,50,0.001) #[kpc]
 G = 13.34*10e-11 #[kpc^3 MO^-1 gy^-2]
 #Newton Dynamic
 omega = sqrt((G*m)/(4*R**3)) #velocities
 vt = omega*R
 plt.xlabel("R [kpc]")
-plt.ylabel("v(t)")
-plt.plot(R,vt, 'r-', label="Newtonian Dynamics")
+plt.ylabel("vt")
+plt.plot(R,omega, 'r-', label="Newtonian Dynamics")
 #MoND
-omega = sqrt((a_0/R)*phi_inverse((m*G)/(4.0*a_0*R**2))) #velocities
+omega = sqrt(G*m)*R**(-(7/2)) #velocities
 vt = omega*R
-plt.plot(R,vt, 'b-', label="Modified Newtonian Dynamics")
+#plt.plot(R,vt, 'b-', label="Modified Newtonian Dynamics")
+plt.plot(R,omega, 'b-', label="Modified Newtonian Dynamics")
 #plt.title("Total Energy of Symplectic Newton Dynamics")
 #filename='./figures/plot.png'
 #plt.savefig(filename)
